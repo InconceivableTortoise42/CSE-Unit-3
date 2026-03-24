647bceb8a80a8ed645d7820b85925bd01451e0b4
InconceivableTortoise42 <redka.1026@foxc6.org>
2026-03-24 08:34:16 -0500

    work on problem page

--- 3.3.1/static/js/practice.js
+++ 3.3.1/static/js/practice.js
@@ -1,6 +1,30 @@
-const mf = document.getElementById("mathField")
-let menuExclusions = ["mode", "color", "background-color", "font-style", "accent", "decoration"]
+const mf = document.getElementById("mathField");
+const apiUrl = `/api/${problemType}`;
+const directionsElement = document.getElementById("directions");
+const menuExclusions = ["mode", "color", "background-color", "accent", "decoration", "variant"];
+
+let directions = {
+    "algebra": "Find the value of x"
+}
+
+function fetchProblem() {
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            directionsElement.innerText = directions[problemType] + data["problem"]
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, "directions"]);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}   
 
 window.onload = () => {
     mf.menuItems = mf.menuItems.filter(item => !menuExclusions.includes(item.id));
+    fetchProblem()
 }
