a2ef67bbadf4f36fe61d81392ae7482758c19d81
Max R <maxrepkar@gmail.com>
2026-03-24 19:25:02 -0500

    styling + functionality

--- 3.3.1/main.py
+++ 3.3.1/main.py
@@ -6,9 +6,10 @@ app.config.from_object('config')
 
 problem_types = {
      "calculus": mathgenerator.calculus.definite_integral,
-     "algebra": mathgenerator.algebra.basic_algebra,
+     "basic_algebra": mathgenerator.algebra.basic_algebra,
      "arithmetic": mathgenerator.basic_math.addition,
-     "geometry": mathgenerator.geometry.circumference
+     "geometry": mathgenerator.geometry.circumference,
+     "combine_like_terms": mathgenerator.algebra.combine_like_terms
 }
 
 @app.route("/")
--- 3.3.1/static/js/practice.js
+++ 3.3.1/static/js/practice.js
@@ -2,12 +2,19 @@ const mf = document.getElementById("mathField");
 const apiUrl = `/api/${problemType}`;
 const directionsElement = document.getElementById("directions");
 const menuExclusions = ["mode", "color", "background-color", "accent", "decoration", "variant"];
+const sumbitButton = document.getElementById("submit");
+const mathProblemElement = document.getElementById("mathProblem");
+const cardHeader = document.getElementById("cardHeader");
+const solution = document.getElementById("solution");
 
 let directions = {
-    "algebra": "Find the value of x"
+    "basic_algebra": "Find the value of x:  ",
+    "combine_like_terms": "Put the expression in it's simplest form: "
 }
 
 function fetchProblem() {
+    mathProblemElement.classList.add("placeholder")
+    mathProblemElement.innerText = "&nbsp;";
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
@@ -16,15 +23,36 @@ function fetchProblem() {
             return response.json();
         })
         .then(data => {
-            directionsElement.innerText = directions[problemType] + data["problem"]
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub, "directions"]);
+            window.setTimeout(() => {
+                mathProblemElement.innerText = data["problem"];
+                solution.innerText = data["solution"]
+                MathJax.typeset([mathProblemElement, solution]);
+                console.log(data["solution"])
+                mathProblemElement.classList.remove("placeholder")
+            }, 1000);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
         });
 }   
 
+function toTitleCase(string) {
+  return string
+    .split(' ') 
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' '); 
+}
+
+sumbitButton.addEventListener("click", (event) => {
+    fetchProblem();
+    console.log(mf.getValue());
+    mf.setValue("");
+});
+
 window.onload = () => {
     mf.menuItems = mf.menuItems.filter(item => !menuExclusions.includes(item.id));
-    fetchProblem()
+    cardHeader.innerText = toTitleCase(problemType.replaceAll("_", " "));
+    document.title = toTitleCase(problemType);
+    directionsElement.innerHTML = directions[problemType] + directionsElement.innerHTML
+    fetchProblem();
 }
